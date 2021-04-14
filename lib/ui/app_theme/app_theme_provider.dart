04b71fcbb789d9ec4df884a65ae1b3aeccfe03cc
Boolean <danielwnewville@gmail.com>
2021-04-13 19:40:35 -0500

    add maintainState

--- lib/ui/app_theme/app_theme_provider.dart
+++ lib/ui/app_theme/app_theme_provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_riverpod_darkswitch/ui/app_theme/app_theme_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final appThemeStateNotifier = StateNotifierProvider<AppThemeState, bool>((ref) => AppThemeState());
+final appThemeStateNotifier = StateNotifierProvider.autoDispose<AppThemeState, bool>((ref) {
+  ref.maintainState = true;
+  return AppThemeState();
+});
