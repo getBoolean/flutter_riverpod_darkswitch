9caef0882ccbb3120f04dff3a475d72891632038
getBoolean <danielwnewville@gmail.com>
2021-04-16 08:57:09 -0500

    change context.read to useProvider to fix the switch not updating its state

--- lib/ui/widgets/dark_mode_switch.dart
+++ lib/ui/widgets/dark_mode_switch.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod_darkswitch/ui/app_theme/app_theme_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class DarkModeSwitch extends StatelessWidget {
+class DarkModeSwitch extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final darkModeEnabled = context.read(appThemeStateNotifier);
+    final darkModeEnabled = useProvider(appThemeStateNotifier);
     return Switch(
       value: darkModeEnabled,
       onChanged: (enabled) {
