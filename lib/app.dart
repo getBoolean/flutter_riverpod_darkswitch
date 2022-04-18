1ff20c5d5cd561eed82693e96c44f1a39ca1e853
getBoolean <danielwnewville@gmail.com>
2022-04-18 13:35:06 -0500

    comments

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,13 @@ import 'ui/home/home_page.dart';
 import 'ui/settings/settings_page.dart';
 
 class MyApp extends HookConsumerWidget {
-  // Cannot be const when using beamer https://github.com/Dart-Code/Dart-Code/issues/3065#issuecomment-1061896753
+  // Cannot be const when using beamer and Dart SDK higher than 2.15 
+  // 
+  // https://github.com/Dart-Code/Dart-Code/issues/3065#issuecomment-1061896753
+  // https://github.com/dart-lang/sdk/issues/47963
+  //
+  // Fix coming soon:
+  // https://github.com/Dart-Code/Dart-Code/issues/3065#issuecomment-1062995190
   MyApp({Key? key}) : super(key: key);
   
   final _routerDelegate = BeamerDelegate(
